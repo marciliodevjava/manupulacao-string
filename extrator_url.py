74657f786447b638d335a3854de9ffa8be2a5bb0
marcsilva <marciliodevjava@gmail.com>
2023-11-27 17:49:36 -0300

    Update fatiamento

--- extrator_url.py
+++ extrator_url.py
@@ -1,4 +1,12 @@
 
-url = 'http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
-
+url = 'bytebank.com/cambio?moedaOrigem=real'
 print(url)
+
+url_divisor = url[19]
+print(url_divisor)
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
