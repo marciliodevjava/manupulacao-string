e6be213be884330778d23a0d9df29c11a04c6e6e
marcsilva <marciliodevjava@gmail.com>
2023-11-29 10:00:38 -0300

    Realizando Fatiamento na url

--- extrator_url.py
+++ extrator_url.py
@@ -1,24 +1,34 @@
 url = 'bytebank.com/cambio?moedaOrigem=real'
-indice_interrogação = url.find('?')
-parametros = indice_interrogação + 1
-tamanho = len(url)
-print('URL Completa: {}'.format(url))
+url = url.strip()
+url = "     "
 
-url_divisor = url[indice_interrogação]
-print(f"Divisor de URL: {url_divisor}")
+# Validação da url
+# Sanitização da url
+url = url.replace(" ", "")
 
-url_base = url[0:indice_interrogação]
-print(f'URL Base: {url_base}')
+if url == "":
+    raise ValueError('Valor da URL está vazia')
+else:
+    indice_interrogação = url.find('?')
+    parametros = indice_interrogação + 1
+    tamanho = len(url)
+    print('URL Completa: {}'.format(url))
 
-cambio = url_base.find('/') + 1
-barra = url_base.find("/")
-print(f'/: {url_base[barra]}')
-url_cambio = url_base[cambio:]
-print(f'Cambio: {url_cambio}')
+    url_divisor = url[indice_interrogação]
+    print(f"Divisor de URL: {url_divisor}")
 
-url_parametros = url[parametros:tamanho]
-print(f'URL Parametros: {url_parametros}')
+    url_base = url[0:indice_interrogação]
+    print(f'URL Base: {url_base}')
 
-real = url_parametros.find('=') + 1
-url_real = url_parametros[real:]
-print(f'Valor Real: {url_real}')
+    cambio = url_base.find('/') + 1
+    barra = url_base.find("/")
+    print(f'/: {url_base[barra]}')
+    url_cambio = url_base[cambio:]
+    print(f'Cambio: {url_cambio}')
+
+    url_parametros = url[parametros:tamanho]
+    print(f'URL Parametros: {url_parametros}')
+
+    real = url_parametros.find('=') + 1
+    url_real = url_parametros[real:]
+    print(f'Valor Real: {url_real}')
