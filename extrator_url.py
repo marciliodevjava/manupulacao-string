060c6ce0b39e6f8323b56779af94fbbf701caf3b
marcsilva <marciliodevjava@gmail.com>
2023-11-29 09:34:16 -0300

    Realizando Fatiamento na url

--- extrator_url.py
+++ extrator_url.py
@@ -2,13 +2,21 @@ url = 'bytebank.com/cambio?moedaOrigem=real'
 indice_interrogação = url.find('?')
 parametros = indice_interrogação + 1
 tamanho = len(url)
-print(url)
+print('URL Completa: {}'.format(url))
 
 url_divisor = url[indice_interrogação]
-print(url_divisor)
+print(f"Divisor de URL: {url_divisor}")
 
 url_base = url[0:indice_interrogação]
-print(url_base)
+print(f'URL Base: {url_base}')
+
+cambio = url_base.find('/') + 1
+url_cambio = url_base[cambio:]
+print(f'Cambio: {url_cambio}')
 
 url_parametros = url[parametros:tamanho]
-print(url_parametros)
+print(f'URL Parametros: {url_parametros}')
+
+real = url_parametros.find('=') + 1
+url_real = url_parametros[real:]
+print(f'Valor Real: {url_real}')
