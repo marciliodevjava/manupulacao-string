1941fcc8ac2738759ef88398234b7f7359d76e33
marcsilva <marciliodevjava@gmail.com>
2023-11-29 13:40:28 -0300

    Criando metodos no extrato_url_new.py

--- extrato_url_new.py
+++ extrato_url_new.py
@@ -21,9 +21,35 @@ class ExtratoUrl:
     def get_url_completa(self):
         return self.__url
 
+    def get_divisor(self):
+        indice_interrogação = self.__url.find('?')
+        return self.__url[indice_interrogação]
+
+    def get_divisor_url(self):
+        divisor = self.__url.find('/')
+        return self.__url[divisor]
+
+    def get_divisor_cambio(self):
+        divisor = self.__url.find('/') + 1
+        cambio = self.__url.find('?')
+        return self.__url[divisor:cambio]
+
+    def get_parametro(self):
+        divisor = self.__url.find('?') + 1
+        return self.__url[divisor:]
+
+    def get_valor(self):
+        divisor = self.__url.find('=') + 1
+        return self.__url[divisor]
+
+
 extrator_url = 'bytebank.com/cambio?moedaOrigem=real'
 
 url = ExtratoUrl(extrator_url)
-
 print(f'URL Completa: {url.get_url_completa()}')
 print('URL Base: {}'.format(url.url_base()))
+print("Divisor ?: {}".format(url.get_divisor()))
+print('Divisor /: {}'.format(url.get_divisor_url()))
+print(f'Cambio: {url.get_divisor_cambio()}')
+print(f'Parametros: {url.get_parametro()}')
+print(f'Valor Cambio: {url.get_valor()}')
