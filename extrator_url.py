9b6e577d055486426ad03fba0dddb0082fbecf5c
marcsilva <marciliodevjava@gmail.com>
2023-11-28 22:15:58 -0300

    Atualizando metodos

--- extrator_url.py
+++ extrator_url.py
@@ -1,13 +1,13 @@
 url = 'bytebank.com/cambio?moedaOrigem=real'
 indice_interrogação = url.find('?')
 parametros = indice_interrogação + 1
-tamanho = len(url + 1)
+tamanho = len(url)
 print(url)
 
 url_divisor = url[indice_interrogação]
 print(url_divisor)
 
-url_base = url[0:19]
+url_base = url[0:indice_interrogação]
 print(url_base)
 
 url_parametros = url[parametros:tamanho]
