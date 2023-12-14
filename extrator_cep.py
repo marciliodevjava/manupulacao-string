e2516d3e96c9d823fa48052f862012cd8c6c0806
marcsilva <marciliodevjava@gmail.com>
2023-12-13 21:43:50 -0300

    Trabalhando com expreção regulares melhorando o metodo

--- extrator_cep.py
+++ extrator_cep.py
@@ -4,7 +4,7 @@ endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Tio de janeiro, RJ,
 
 # 5 digitos +- hifen(opcional)
 
-padrao_cep = re.compile("[0123456789]{5}[-]?[0123456789]{3}")
+padrao_cep = re.compile("[0-9]{5}[-]?[0-9]{3}")
 
 buscar = padrao_cep.search(endereco)
 
